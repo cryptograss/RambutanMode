af49eb666ffd7c3934860b4011287e447d77e942
Justin Holmes <justin@justinholmes.com>
2026-01-19 05:39:45 +0000

    Add plain text parser functions for use inside wikilinks
    {{#rambutanplain:Name}} and {{#rambutanbandplain:Name}} return
    rambutan-modified text without wikilinks, suitable for use as
    display text inside other wikilinks (since MediaWiki doesn't
    support nested wikilinks).

--- i18n/RambutanMode.magic.php
+++ i18n/RambutanMode.magic.php
@@ -8,4 +8,6 @@ $magicWords = [];
 $magicWords['en'] = [
     'rambutan' => [ 0, 'rambutan' ],
     'rambutanband' => [ 0, 'rambutanband' ],
+    'rambutanplain' => [ 0, 'rambutanplain' ],
+    'rambutanbandplain' => [ 0, 'rambutanbandplain' ],
 ];
